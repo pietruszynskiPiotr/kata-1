79515a2bb2645dc6a80cc3f8ababfd1478cf5a08
Piotr Pietruszyński <pietruszynski.piotr@gmail.com>
2021-10-07 17:46:44 +0200

    kata 1 version 2

--- src/es/ulpgc/Main.java
+++ src/es/ulpgc/Main.java
@@ -1,11 +1,16 @@
 package es.ulpgc;
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Main {
 
     public static void main(String[] args) {
-        Person person = new Person("Blas", new Date(93, 8, 2));
+        Calendar date;
+        date = GregorianCalendar.getInstance();
+        date.set(1993, 8, 2);
+        Person person = new Person("Blas", date);
         System.out.println(person.getName() + " tiene " + person.getAge());
     }
+
 }
--- src/es/ulpgc/Person.java
+++ src/es/ulpgc/Person.java
@@ -1,14 +1,17 @@
 package es.ulpgc;
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Person {
 
     private final String name;
 
-    private final Date birthday;
+    private final Calendar birthday;
 
-    public Person(String name, Date birthDay) {
+    private final long MILLISECONDS_PER_YEAR = (long) (1000 * 60 * 60 * 24 * 365.25);
+
+    public Person(String name, Calendar birthDay) {
         this.name = name;
         this.birthday = birthDay;
     }
@@ -17,11 +20,17 @@ public class Person {
         return name;
     }
 
-    public Date getBirthday() {
+    public Calendar getBirthday() {
         return birthday;
     }
 
     public int getAge() {
-        return (int) ((new Date().getTime() - birthday.getTime())/31536000000L);
+        Calendar today = GregorianCalendar.getInstance();
+        return (int) (milliSecondsToYear(today.getTimeInMillis() - birthday.getTimeInMillis()));
     }
+
+    private long milliSecondsToYear(long milles) {
+        return milles / MILLISECONDS_PER_YEAR;
+    }
+
 }
